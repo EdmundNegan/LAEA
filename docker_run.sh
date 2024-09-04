09e51f238fbf43662f4f6dead822035342ec3dc1
EdmundNegan <zceenga@ucl.ac.uk>
2024-09-04 15:26:32 +0800

    LAEA Working

---
+++ docker_run.sh
@@ -0,0 +1,17 @@
+xhost local:docker &&
+docker run -it --rm \
+    --privileged \
+    --gpus all \
+    --net=host \
+    --ipc host \
+    --name laea \
+    -e "DISPLAY=$DISPLAY" \
+    -e "QT_X11_NO_MITSHM=1" \
+    -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -v "$HOME/.Xauthority:/root/.Xauthority" \
+    -v "/home/intern/LAEA:/root/ros_ws/src/LAEA" \
+    ros_noetic_image:v1.5 \
+    bash -c "
+    cd ~/ros_ws && source devel/setup.bash; 
+    cd .. && source .bashrc;
+    bash"
